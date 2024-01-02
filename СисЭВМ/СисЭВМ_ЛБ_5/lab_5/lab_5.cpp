6000399ef3d64699f00d5414c8527d2490f3f347
Lion120454 <117920260+Lion120454@users.noreply.github.com>
2024-01-02 23:35:14 +0700

    Add files via upload

---
+++ СисЭВМ/СисЭВМ_ЛБ_5/lab_5/lab_5.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+const int hash_size = 20;
+
+struct Hash 
+{
+    int data; //ключ
+    int index; //индекс
+    int tries; //попытки
+};
+Hash hash_array[hash_size];
+
+void init()//Инициализация
+{
+    for (int i = 0; i < hash_size; i++) 
+    {
+        hash_array[i].tries = 0;
+        hash_array[i].index = i;
+    }
+}
+
+int getHash(int number) 
+{
+    return (number % hash_size);
+}
+
+void Add(int Index, int number)//Добавление
+{
+    bool found = false;
+    int i = 0;
+    while (i != hash_size) 
+    {
+        Index = (number + i) % hash_size;
+
+        if (hash_array[Index].data == 0 || hash_array[Index].data == -1)
+        {
+            hash_array[Index].data = number;
+            hash_array[Index].tries = i;
+            found = true;
+            break;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    if (found != true) 
+    {
+        cout << "Хеш-таблица заполнена" << endl;
+    }
+}
+
+void Print()//Вывод
+{
+    cout << "Хэш-Таблица:" << endl;
+    cout << "Индекс " <<  " Ключ  " << "Попытки:" << endl;
+    for (int i = 0; i < hash_size; i++) 
+    {
+        cout << "---------------------" << endl;
+        cout << "   " << hash_array[i].index << "    " << hash_array[i].data << "      " << hash_array[i].tries << endl;
+    }
+}
+
+void Search(int number)//Поиск
+{
+    int i = 0;
+    bool found = false;
+    while (i != hash_size)
+    {
+        int index = (number + i) % hash_size;
+        if (hash_array[index].data == number)
+        {
+            cout << "Число " << number << " найдено" << endl;
+            cout << "Кол-во попыток при поиске "<<i << endl;
+            found = true;
+            break;
+        }
+        else if (hash_array[index].data == 0)
+        {
+            break;
+        }
+        i++;
+    }
+    if (found != true) 
+    {
+        cout << "Число " << number << " не найдено" << endl;
+    }
+}
+
+void Delete(int number)//Удаление
+{
+    int i = 0;
+    bool found = false;
+    while (i != hash_size)
+    {
+        int index = (number + i) % hash_size;
+        if (hash_array[index].data == number)
+        {
+            hash_array[index].data = -1;
+            hash_array[index].tries = -1;
+            cout << "Число "<< number << " удалено" << endl;
+            found = true;
+            break;
+        }
+        i++;
+    }
+    if (found != true) 
+    {
+        cout << "Число " << number << " не найдено" << endl;
+    }
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "RUS");
+    int number, Index;
+    init();
+    ifstream File("IN.txt");
+    if (File.is_open()) 
+    {
+        while (File.eof() == 0) 
+        {
+            File >> number;
+            Index = getHash(number);
+            Add(Index, number);
+        }
+        File.close();
+        Print();
+    }
+    else 
+    {
+        cout << "Файл не открыт";
+    }
+    cout << endl;
+    Search(1+rand()%100);
+    cout << endl;
+    Delete(42);
+    Print();
+}
---
+++ СисЭВМ/СисЭВМ_ЛБ_6/lab_6/lab_6.cpp
@@ -0,0 +1,227 @@
+﻿#include <iostream> 
+#include <fstream> 
+#include <iomanip> 
+
+using namespace std;
+
+struct Node
+{
+    int ver1, ver2;
+    Node* next;
+    Node(int _ver1, int _ver2)
+    {
+        ver1 = _ver1;
+        ver2 = _ver2;
+        next = nullptr;
+    }
+};
+struct List
+{
+    Node* first;
+    Node* last;
+
+    List()//Инициализация 
+    {
+        first = nullptr;
+        last = nullptr;
+    }
+    bool isEmpty()//Проверка на пустоту 
+    {
+        if (first == nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void PushBack(int _ver1, int _ver2)//Добавление 
+    {
+        Node* p = new Node(_ver1, _ver2);
+
+        if (isEmpty() == true)
+        {
+            first = p;
+            last = p;
+            return;
+        }
+
+        last->next = p;
+        last = p;
+    }
+    void DeleteFirst()//Удаление 
+    {
+        if (isEmpty() == true)
+        {
+            return;
+        }
+
+        Node* p = first;
+        first = p->next;
+        delete p;
+    }
+};
+
+struct NodeQ
+{
+    int data;
+    NodeQ* next;
+};
+struct Queue
+{
+    NodeQ* firstQ;
+    NodeQ* lastQ;
+
+    bool isEmpty(Queue* q)//Проверка очереди на пустоту 
+    {
+        if (q->firstQ == nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void init(Queue* q)//Инициализация очереди 
+    {
+        q->firstQ = nullptr;
+        q->lastQ = nullptr;
+    }
+    List push(Queue* q, List l, int data, bool* visited)//Вставка нового элемента в очередь 
+    {
+        if (l.first->ver1 == data)
+        {
+            NodeQ* k = new NodeQ;
+
+            if (k == nullptr)
+            {
+                cout << "k = nullptr" << endl;
+                exit(-1);
+            }
+            if (q->firstQ == nullptr)
+            {
+                k->data = 1;
+                visited[0] = true;
+            }
+            else if (visited[l.first->ver2 ] == false)
+            {
+                visited[l.first->ver2] = true;
+                k->data = l.first->ver2;
+                cout << l.first->ver1 << " -> " << l.first->ver2 << endl;
+                l.DeleteFirst();
+            }
+            else
+            {
+                l.DeleteFirst();
+                return l;
+            }
+
+            k->next = nullptr;
+
+            if (q->firstQ == nullptr)
+            {
+                q->firstQ = k;
+            }
+            else
+            {
+                q->lastQ->next = k;
+            }
+            q->lastQ = k;
+
+            return l;
+        }
+        else
+        {
+            l.DeleteFirst();
+            return l;
+        }
+    }
+    int top(Queue* q)//Вернуть первый элемент очереди 
+    {
+        if (isEmpty(q) == true)
+        {
+            cout << "Очередь пуста" << endl;
+            exit(0);
+        }
+        else
+        {
+            return q->firstQ->data - 1;
+        }
+    }
+    void pop(Queue* q)
+    {
+        if (isEmpty(q) == 1)
+        {
+            cout << "Очередь пуста" << endl;
+        }
+        else
+        {
+            NodeQ* p = new NodeQ;
+            p = q->firstQ;
+            q->firstQ = p->next;
+
+            if (q->firstQ == nullptr)
+            {
+                q->lastQ = nullptr;
+            }
+
+            delete p;
+        }
+    }
+};
+List File() 
+{
+    ifstream File("input.txt");
+    if (File.is_open())
+    {
+        List l;
+        int ver1, ver2;
+        while (!File.eof())
+        {
+            File >> ver1 >> ver2;
+            l.PushBack(ver1, ver2);
+
+        }
+        return l;
+    }
+    else
+    {
+        cout << "Файл не открыт";
+        exit(-1);
+    }
+}
+void BFS(List graph)
+{
+    int z;
+    Queue q;
+    q.init(&q);
+    bool* visited = new bool[5];
+    for (int i = 0; i < 5; i++)
+    {
+        visited[i] = false;
+    }
+
+    z = 0;
+    graph = q.push(&q, graph, z + 1, visited);
+    while (q.isEmpty(&q) == false)
+    {
+        z = q.top(&q);
+        while (graph.first->next != nullptr)
+        {
+            graph = q.push(&q, graph, z + 1, visited);
+        }
+        graph = File();
+        q.pop(&q);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    List graph=File();
+    cout << "Ребра графа, вошедшие в стягивающее дерево:" << endl;
+    BFS(graph);
+    return 0;
+}

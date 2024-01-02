a40ee428259375f2ff573255c3a29b9524c884bf
Lion120454 <117920260+Lion120454@users.noreply.github.com>
2024-01-02 23:25:08 +0700

    Add files via upload

---
+++ ВТ/ВТ_ЛБ_4/lb_4.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include "locale.h"
+
+using namespace std;
+
+int prime(int n)
+{
+    if (n < 2 || n % 2 == 0)
+    {
+        return 0;
+    } 
+    for (int i = 3; i * i <= n; i = i+ 2) 
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    } 
+    return 1;
+}
+int main()
+{
+    int x;
+    setlocale(LC_ALL, "Ru");
+    int count = 0,flag = 0, namber = 0;
+    for (x = 1; x <= 90000; x++)
+    {
+        if (prime(2 * x - 1) == 1)
+        {
+            count++;
+            if (count==200)
+            {
+                namber = 2 * x - 1;
+            }
+            /*if (count <= 200)
+            {
+                cout << count << " = " << 2 * x - 1 << endl;
+            }*/
+        }
+    }
+    cout << "1#Кол= " << count << endl;
+    cout << "2#200= " << namber << endl;
+    //--------------------------------------------------------------------------------------------------------------
+    
+    count = 0;
+    namber=0;
+    int x2;
+    _asm
+    {
+        mov EAX, 0; //Помещаем в регистр EAX 0
+        mov dword ptr[x2], 1 // x2=1
+        mov ECX, 90000 //Счётчик ECX =90000
+        nextX: //цикл
+        call Prime
+            add dword ptr[x2], 2 //x2+2
+            loop nextX //вызов цикла
+            jmp quit
+
+            Prime :
+        cmp dword ptr[x2], 2 //сравниваем x2 и 2
+            je prm //если х2=2 переходим prm
+            ja jmp1 //если х2>2 переходим jmp1
+            ret //Выход
+
+            jmp1 : // исключаем все чётные числа
+        or byte ptr[x2], 1 //Проверка на чётность
+            jnz jmp2 //Если нечётное переходим jmp2
+            ret //Выход
+
+            jmp2 :
+        mov EBX, 3 //EBX =3
+
+            jmp4 :
+            mov EAX, EBX //EAX=EBX
+            mul EAX //EAX*EAX
+            cmp EAX, x2//Сравниваем EAX и x2
+            ja prm //если 9>x2 переходим prm
+            mov EAX, x2 //EAX=x2
+            div EBX// EAX/EBX
+            cmp EDX, 0 //Сравниваем остаток EDX и 0
+            jnz jmp3 //Если не равны переходим в jmp3
+            ret //Выход
+
+            jmp3 :
+        add EBX, 2 //EBX+2
+            jmp jmp4 //Без условный переход в jmp4
+
+            prm :
+        add dword ptr[count], 1 //n+1
+            cmp count,200
+            je save //
+            ret //Выход
+
+            save:
+        mov EAX,x2 //EAX=x2
+            add namber,EAX //namber=namber+EAX
+            ret //Вывод
+
+            quit ://выход
+    }
+    cout << "--------------------"<< endl;
+    cout << "ASM Кол= " << count << endl;
+    cout << "ASM 200= " << namber << endl;
+    return 1;
+}
---
+++ ВТ/ВТ_ЛБ_5/АААААААААААААААААААААААААААА/АААААААААААААААААААААААААААА.cpp
@@ -0,0 +1,244 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h> 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int time_cpp = 0;
+	int time_scal = 0;
+	int time_vec = 0;
+	time_t start;
+	FILE* in, * out;
+	unsigned __int8* buffer;
+	unsigned __int32 wight, height;
+	//Блок C++
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "Loadeing 0: " << i <<"%" << endl;
+		start = clock();//Старт замера
+		fopen_s(&in, "in.bmp", "rb");
+		buffer = (unsigned __int8*)malloc(54);
+		fread(buffer, 1, 54, in);
+		for (int i = 0; i <= 3; i++)
+		{
+			wight = buffer[21 - i] | wight << 8;
+			height = buffer[25 - i] | height << 8;
+		}
+		fopen_s(&out, "OUT_cpp.bmp", "wb");
+		fwrite(buffer, 1, 54, out);
+		free(buffer);
+
+		//Редактирование картинки
+		buffer = (unsigned __int8*)malloc(wight * height * 4);
+		fread(buffer, 1, wight * height * 4, in);
+		fclose(in);
+
+		for (int i = 0; i < wight; i += 128)
+		{
+			for (int j = height; j > height / 2; j--)
+			{
+				for (int k = i; k < wight; k++)
+				{
+					buffer[wight * 3 * j + k * 3 + 2] = 255;
+					buffer[wight * 3 * j + k * 3 + 1] = 0;
+					buffer[wight * 3 * j + k * 3 + 0] = 0;
+				}
+			}
+		}
+		fwrite(buffer, 1, wight * height * 4, out);
+		fclose(out);
+		free(buffer);
+		time_cpp += clock() - start;//Конц замера
+	}
+	cout << endl;
+	//Блок ASM_SCAL----------------------------------------------------------------------
+	in = NULL;
+	out = NULL;
+	buffer = 0;
+	wight = 0;
+	height = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "Loadeing 1: " << i << "%" << endl;
+		start = clock();
+		fopen_s(&in, "in.bmp", "rb");
+		buffer = (unsigned __int8*)malloc(54);
+		fread(buffer, 1, 54, in);
+		__asm
+		{
+			mov ESI, buffer;			//Указатель на массив(регистр индекса источника)
+			mov ECX, 3;					//Начинаем извлекать высоту и ширину
+			mov EBX, 18;
+			mov EAX, [ESI + EBX];
+			mov wight, EAX;
+			mov EAX, [ESI + EBX + 4];
+			mov height, EAX;
+		}
+		fopen_s(&out, "OUT_scal.bmp", "wb");
+		fwrite(buffer, 1, 54, out);
+		free(buffer);
+
+		buffer = (unsigned __int8*)malloc(wight * height * 4);
+		fread(buffer, 1, wight * height * 4, in);
+		fclose(in);
+
+		__asm
+		{
+			mov ESI, buffer;//Получаем адресс из буфера
+			mov ECX, wight;//Делаем счётчик для 1 цикла
+		l1:
+			push ECX;//Убираем счётчик в стек
+			mov ECX, 300;//Делаем новый счётчик для 2 цикла
+		l2:
+			mov EAX, 3;//Подготавливаем часть индекса, который используется в 3 цикле (wight*3*j)
+			mul wight;
+			mov EBX, height;
+			sub EBX, ECX;
+			mul EBX;
+			mov EBX, EAX;
+			//Подготавливаем остальную часть индекса(+i*3). Тк там используется значения из 1 цикла, делаем махинации с стеком
+			mov EAX, ECX;	//Сохраняем текущее значение счётчика
+			pop ECX;		//Достаём значения счётчика с прошлого цикла
+			push ECX;		//Тк нам понадобится eax, сохраняем значения текущего счётчика в стеке
+
+			mov EAX, wight;
+			sub EAX, ECX;
+			mov EDX, 3;
+			mul EDX;
+			add EBX, EAX;
+			mov EAX, 0;
+			//Возвращаем значения счётчика 1 цикла в стек и достаём значения текущего
+			pop EAX;
+			push ECX;
+			mov ECX, EAX;
+			mov EAX, 0;
+				
+			push ECX;
+			mov ECX, wight;
+		l3:
+			add EBX, 2;
+			mov[ESI + EBX], 255;//Освещаем пиксель
+			inc EBX;
+			loop l3;
+					
+			pop ECX;		//Забираем счётчик 2 цикла из стека
+			loop l2;
+				
+			pop ECX;		//Забираем счётчик 3 цикла из стека
+			sub ECX, 127;	//Смещаемся не на 1 пиксель, а на 64*2-1, тк взаимодействие с 64 нужными было в 3 цикле
+			cmp ECX, 0;		//Если наше смещение привело к отрицательному(или равному нулую) ecx - выходим из 1 цикла
+			jle ex;
+			loop l1;
+		ex:	
+		}
+		fwrite(buffer, 1, wight * height * 4, out);
+		fclose(out);
+		free(buffer);
+		time_scal += clock() - start;
+	}
+	cout << endl;*/
+	//Блок ASM_VEC----------------------------------------------------------------------
+	
+	in = NULL;
+	out = NULL;
+	buffer = NULL;
+	wight = 0;
+	height = 0;
+	for(int i=0;i<100;i++)
+	{
+		cout << "Loadeing 2: " << i << "%" << endl;
+		start = clock();
+		in = NULL;
+		out = NULL;
+		buffer = NULL;
+		wight = 0;
+		height = 0;
+
+		fopen_s(&in, "in.bmp", "rb");
+		buffer = (unsigned __int8*)malloc(54);
+		fread(buffer, 1, 54, in);
+			
+		__asm
+		{
+			mov ESI, buffer;			//Указатель на массив
+			mov ECX, 3;					//Начинаем извлекать высоту и ширину
+			mov EBX, 18;
+			mov EAX, [ESI + EBX];
+			mov wight, EAX;
+			mov EAX, [ESI + EBX + 4];
+			mov height, EAX;
+		}
+		fopen_s(&out, "OUT_vec.bmp", "wb");
+		fwrite(buffer, 1, 54, out);
+		free(buffer);
+	
+		buffer = (unsigned __int8*)malloc(wight * height * 4);
+		fread(buffer, 1, wight* height * 4, in);
+		fclose(in);
+			
+		__asm
+		{
+			mov ESI, buffer;//Получаем адресс из буфера
+			mov ECX, wight;//Делаем счётчик для 1 цикла
+		l11:
+			push ECX;//Убираем счётчик в стек
+			mov ECX, 300;//Делаем новый счётчик для 2 цикла
+		l12:
+			mov EAX, 3;//Подготавливаем часть индекса, который используется в 3 цикле (wight*3*j)
+			mul wight;
+			mov EBX, height;
+			sub EBX, ECX;
+			mul EBX;
+			mov EBX, EAX;
+			//Подготавливаем остальную часть индекса(+i*3). Тк там используется значения из 1 цикла, делаем махинации с стеком
+			mov EAX, ECX;	//Сохраняем текущее значение счётчика
+			pop ECX;		//Достаём значения счётчика с прошлого цикла
+			push EAX;		//Тк нам понадобится EAX, сохраняем значения текущего счётчика в стеке
+				
+			mov EAX, wight;
+			sub EAX, ECX;
+			mov EDX, 3;
+			mul EDX;
+			add EBX, EAX;
+			mov EAX, 0;
+			//Возвращаем значения счётчика 1 цикла в стек и достаём значения текущего
+			pop EAX;
+			push ECX;
+			mov ECX, EAX;
+			mov EAX, 0;
+				
+			push ECX;
+			mov ECX, wight;
+		l13:
+			//Освещаем пиксель
+			add EBX, 3;
+			movd mm0, [ESI + EBX];//запись в регистр MMX 
+			mov EAX, 0x00FF0000;
+			movd mm1, EAX;//запись в регистр MMX 
+			paddusb mm0, mm1;//Сложение с насыщением беззнаковых упакованных байт
+			movd[ESI + EBX], mm0;
+			//Освещаем пиксель
+			loop l13;
+				
+			pop ECX;		//Забираем счётчик 2 цикла из стека
+			loop l12;
+
+			pop ECX;		//Забираем счётчик 1 цикла из стека
+			sub ECX, 127;	//
+			cmp ECX, 0;		//Если наше смещение привело к отрицательному(или равному нулую) ecx - выходим из 1 цикла
+			jle ex1;
+			loop l11;
+		ex1:
+		}
+		fwrite(buffer, 1, wight* height * 4, out);
+		fclose(out);
+		free(buffer);
+		time_vec += clock() - start;
+	}
+	cout << "Время работы алгоритма на C++:" << time_cpp << endl;
+	cout << "Время работы алгоритма на ASM c использованием скаляров:" << time_scal << endl;
+	cout << "Время работы алгоритма на ASM с использованием векторов:" << time_vec << endl;
+}
